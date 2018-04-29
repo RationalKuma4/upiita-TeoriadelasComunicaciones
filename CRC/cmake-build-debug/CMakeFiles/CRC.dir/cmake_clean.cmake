5cac30d36e13e5df852ba248b98628a6f874fbda
Anselmo Alvarado <RationalKuma4@users.noreply.github.com>
2018-04-29 13:21:01 -0500

    CRC

---
+++ CRC/cmake-build-debug/CMakeFiles/CRC.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CRC.dir/main.cpp.obj"
+  "CRC.pdb"
+  "CRC.exe"
+  "CRC.exe.manifest"
+  "libCRC.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CRC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
